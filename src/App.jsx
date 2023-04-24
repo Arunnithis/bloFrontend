1118ccae826921a99f9f7a97ef777448ec8aca54
Arunnithis <arunnithis24@gmail.com>
2023-04-24 12:33:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/home/Homepage";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Setting from "./pages/settings/Setting";
+import Sidebar from "./components/sidebar/Sidebar";
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Link} from 'react-router-dom';import { useContext } from "react";
+import { Context } from "./context/Context";
+import About from "./pages/about/About";
+;
+
+function App() {
+
+  const {user} = useContext(Context);
+
+  return (
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/posts" element={<Homepage />} />
+        <Route path="/register" element={user ? <Homepage /> : <Register />} />
+        <Route path="/login" element={user ? <Homepage /> : <Login />} />
+        <Route path="/post/:id" element={<Single />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/write" element={user ? <Write /> : <Login />} />
+        <Route path="/settings" element={user ? <Setting /> : <Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
